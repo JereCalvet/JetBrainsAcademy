4e7620ad30ba897d5691594350751f600a0fd248
Jereaaa <jere_calvet@hotmail.com>
2021-02-03 18:07:20 -0300

    borrado

--- Web Quiz Engine/Web Quiz Engine/task/src/engine/models/quizdomain/completedrecord/QuizCompletedLogDao.java
+++
@@ -1,8 +0,0 @@
-package engine.models.quizdomain.completedrecord;
-
-import org.springframework.data.domain.Page;
-
-public interface QuizCompletedLogDao {
-    QuizCompleted save(QuizCompleted quizCompleted);
-    Page<QuizCompleted> findAllById(Long id, int pageNo);
-}
--- Web Quiz Engine/Web Quiz Engine/task/src/engine/models/userdomain/UserDao.java
+++
@@ -1,9 +0,0 @@
-package engine.models.userdomain;
-
-
-import java.util.Optional;
-
-public interface UserDao {
-    Optional<User> selectByEmail(String email);
-    User save(User user);
-}
